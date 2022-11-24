4f296afed63d2e2e242a79c452eead24300d0650
Karl Koscher <kkoscher@octoml.ai>
2022-11-23 17:37:34 -0800

    Restore update_content_hexagon.sh

--- .devcontainer/update_content_hexagon.sh
+++ .devcontainer/update_content_hexagon.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-exit
-
 cmake -S apps/hexagon_api -B apps/hexagon_api/build \
   -DUSE_ANDROID_TOOLCHAIN=$HEXAGON_SDK_ROOT/android-ndk-r19c/build/cmake/android.toolchain.cmake \
   -DANDROID_PLATFORM=android-28 -DANDROID_ABI=arm64-v8a -DUSE_HEXAGON_ARCH=v69 \
